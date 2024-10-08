5ce8407544793060908c799f07b41f884c511a9d
fayeemismail <faheemmuhammed703@gmail.com>
2024-10-08 20:47:31 +0530

    min_max_done

--- binaryTree.js
+++ binaryTree.js
@@ -179,3 +179,130 @@
 // bst.postOrder(bst.root)
 // console.log('----------BFS-----------')
 // bst.levelOrder()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+class binarySearchTree {
+    constructor(){
+        this.root = null
+    }
+    insert(value) {
+        const node = new Node(value)
+        if(this.root == null){
+            this.root = node
+        } else {
+            this.insertNode(this.root, node)
+        }
+    }
+    insertNode(root, node){
+        if(root.value > node.value){
+            if(root.left == null){
+                root.left = node
+            } else {
+                this.insertNode(root.left, node)
+            }
+        } else {
+            if(root.right == null){
+                root.right = node
+            } else {
+                this.insertNode(root.right,node)
+            }
+        }
+    }
+    search(root,value){
+        if(!root){
+            return false
+        } else {
+            if(root.value == value){
+                return true
+            } else if (root.value > value){
+                return this.search(root.left, value)
+            } else {
+                return this.search(root.right, value)
+            }
+        }
+    }
+    
+    preOrder(root){
+        if(root){
+            console.log(root.value, 'pre')
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+    
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value, 'in')
+            this.inOrder(root.right)
+        }
+    }
+    
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value, 'post')
+        }
+    }
+    
+    min(root){
+        if(!root.left){
+            return root.value
+        } else {
+            return this.min(root.left)
+        }
+    }
+    max(root){
+        if(!root.right){
+            return root.value
+        } else {
+            return this.max(root.right)
+        }
+    }
+}
+
+let bst = new binarySearchTree()
+bst.insert(10)
+bst.insert(15)
+bst.insert(5)
+bst.insert(3)
+bst.insert(7)
+
+console.log(bst.search(bst.root,5))
+bst.preOrder(bst.root)
+bst.inOrder(bst.root)
+bst.postOrder(bst.root)
+
+console.log('------------')
+console.log(bst.min(bst.root))
+console.log(bst.max(bst.root))
