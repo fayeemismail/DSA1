4d15ebd5095b65d099c495eee9192901fb62fc50
fayeemismail <faheemmuhammed703@gmail.com>
2024-10-08 19:55:25 +0530

    done_dfs

--- binaryTree.js
+++ binaryTree.js
@@ -43,9 +43,9 @@
 //             if(root.value == value){
 //                 return true
 //             } else if (root.value > value) {
-//                 return search(root.left, value)
+//                 return this.search(root.left, value)
 //             } else {
-//                 return search(root.right, value)
+//                 return this.search(root.right, value)
 //             }
 //         }
 //     }
@@ -58,4 +58,124 @@
 // bst.insert(15)
 // bst.insert(5)
 
-// console.log(bst.search(5))
+// console.log(bst.search(bst.root, 20))
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------BFS and DFS-------------------------------------------------------
+
+
+
+
+
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+class binarySearchTree {
+    constructor(){
+        this.root = null
+    }
+    insert(value){
+        const node = new Node(value)
+        if(this.root == null){
+            this.root = node
+        } else {
+            this.insertNode(this.root, node)
+        }
+    }
+    insertNode(root,node){
+        if(root.value > node.value){
+            if(root.left == null){
+                root.left = node
+            } else {
+                this.insertNode(root.left, node)
+            }
+        } else {
+            if(root.right == null){
+                root.right = node
+            } else {
+                this.insertNode(root.right, node)
+            }
+        }
+    }
+    search(root, value){
+        if(!root){
+            return false
+        } else {
+            if(root.value == value){
+                return true
+            } else if (root.value > value){
+                return this.search(root.left, value)
+            } else {
+                return this.search(root.right, value)
+            }
+        }
+    }
+    
+    preOrder(root){
+        if(root){
+            console.log(root.value)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+    
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value)
+            this.inOrder(root.right)
+        }
+    }
+    
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value)
+        }
+    }
+    
+    levelOrder(){
+        let queue = []
+        queue.push(this.root)
+        while(queue.length){
+            let current = queue.shift()
+            console.log(current.value)
+            if(current.left){
+                queue.push(current.left)
+            }
+            if(current.right){
+                queue.push(current.right)
+            }
+        }
+    }
+}
+
+const bst = new binarySearchTree()
+bst.insert(10)
+bst.insert(15)
+bst.insert(5)
+bst.insert(3)
+bst.insert(7)
+
+console.log(bst.search(bst.root,20))
+bst.preOrder(bst.root)
+console.log('------------------')
+bst.inOrder(bst.root)
+console.log('------------------')
+bst.postOrder(bst.root)
+console.log('----------BFS-----------')
+bst.levelOrder()
