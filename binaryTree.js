ae5baf70341d8f6d5b6c33b4745a054d95f5c0ff
fayeemismail <faheemmuhammed703@gmail.com>
2024-10-13 17:37:05 +0530

    code_simplified_binaryTree

--- binaryTree.js
+++ binaryTree.js
@@ -5,18 +5,17 @@
 //         this.right = null
 //     }
 // }
-// class BinarySearchTree {
+
+// class binarySearchTree {
 //     constructor(){
 //         this.root = null
 //     }
-//     isEmpty(){
-//         return this.root == null
-//     }
+    
 //     insert(value){
 //         const node = new Node(value)
 //         if(this.root == null){
 //             this.root = node
-//         }else{
+//         } else {
 //             this.insertNode(this.root, node)
 //         }
 //     }
@@ -24,8 +23,8 @@
 //         if(root.value > node.value){
 //             if(root.left == null){
 //                 root.left = node
-//             }else{
-//                 this.insertNode(root.left, node)
+//             } else {
+//                 this.insertNode(root.left , node)
 //             }
 //         } else {
 //             if(root.right == null){
@@ -35,209 +34,6 @@
 //             }
 //         }
 //     }
-    
-//     search(root, value){
-//         if(!root){
-//             return false
-//         } else {
-//             if(root.value == value){
-//                 return true
-//             } else if (root.value > value) {
-//                 return this.search(root.left, value)
-//             } else {
-//                 return this.search(root.right, value)
-//             }
-//         }
-//     }
-// }
-
-// const bst = new BinarySearchTree()
-// console.log(bst.isEmpty())
-
-// bst.insert(10)
-// bst.insert(15)
-// bst.insert(5)
-
-// console.log(bst.search(bst.root, 20))
-
-
-
-
-
-
-
-
-
-
-
-//--------------------------------------------------------BFS and DFS-------------------------------------------------------
-
-
-
-
-
-// class Node {
-//     constructor(value){
-//         this.value = value
-//         this.left = null
-//         this.right = null
-//     }
-// }
-// class binarySearchTree {
-//     constructor(){
-//         this.root = null
-//     }
-//     insert(value){
-//         const node = new Node(value)
-//         if(this.root == null){
-//             this.root = node
-//         } else {
-//             this.insertNode(this.root, node)
-//         }
-//     }
-//     insertNode(root,node){
-//         if(root.value > node.value){
-//             if(root.left == null){
-//                 root.left = node
-//             } else {
-//                 this.insertNode(root.left, node)
-//             }
-//         } else {
-//             if(root.right == null){
-//                 root.right = node
-//             } else {
-//                 this.insertNode(root.right, node)
-//             }
-//         }
-//     }
-//     search(root, value){
-//         if(!root){
-//             return false
-//         } else {
-//             if(root.value == value){
-//                 return true
-//             } else if (root.value > value){
-//                 return this.search(root.left, value)
-//             } else {
-//                 return this.search(root.right, value)
-//             }
-//         }
-//     }
-    
-//     preOrder(root){
-//         if(root){
-//             console.log(root.value)
-//             this.preOrder(root.left)
-//             this.preOrder(root.right)
-//         }
-//     }
-    
-//     inOrder(root){
-//         if(root){
-//             this.inOrder(root.left)
-//             console.log(root.value)
-//             this.inOrder(root.right)
-//         }
-//     }
-    
-//     postOrder(root){
-//         if(root){
-//             this.postOrder(root.left)
-//             this.postOrder(root.right)
-//             console.log(root.value)
-//         }
-//     }
-    
-//     levelOrder(){
-//         let queue = []
-//         queue.push(this.root)
-//         while(queue.length){
-//             let current = queue.shift()
-//             console.log(current.value)
-//             if(current.left){
-//                 queue.push(current.left)
-//             }
-//             if(current.right){
-//                 queue.push(current.right)
-//             }
-//         }
-//     }
-// }
-
-// const bst = new binarySearchTree()
-// bst.insert(10)
-// bst.insert(15)
-// bst.insert(5)
-// bst.insert(3)
-// bst.insert(7)
-
-// console.log(bst.search(bst.root,20))
-// bst.preOrder(bst.root)
-// console.log('------------------')
-// bst.inOrder(bst.root)
-// console.log('------------------')
-// bst.postOrder(bst.root)
-// console.log('----------BFS-----------')
-// bst.levelOrder()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Node {
-//     constructor(value){
-//         this.value = value
-//         this.left = null
-//         this.right = null
-//     }
-// }
-// class binarySearchTree {
-//     constructor(){
-//         this.root = null
-//     }
-//     insert(value){
-//         const node = new Node(value)
-//         if(this.root == null){
-//             this.root = node
-//         } else {
-//             this.insertNode(this.root, node)
-//         }
-//     }
-//     insertNode(root,node){
-//         if(root.value > node.value){
-//             if(root.left == null){
-//                 root.left = node
-//             } else {
-//                 this.insertNode(root.left, node)
-//             }
-//         } else {
-//             if(root.right == null){
-//                 root.right = node
-//             } else {
-//                 this.insertNode(root.right, node)
-//             }
-//         }
-//     }
-    
 //     search(root,value){
 //         if(!root){
 //             return false 
@@ -245,13 +41,12 @@
 //             if(root.value == value){
 //                 return true
 //             } else if (root.value > value){
-//                 return this.search(root.left, value)
+//                 return this.search(root.left , value)
 //             } else {
 //                 return this.search(root.right, value)
 //             }
 //         }
 //     }
-    
 //     preOrder(root){
 //         if(root){
 //             console.log(root.value)
@@ -266,7 +61,6 @@
 //             this.inOrder(root.right)
 //         }
 //     }
-    
 //     postOrder(root){
 //         if(root){
 //             this.postOrder(root.left)
@@ -274,11 +68,10 @@
 //             console.log(root.value)
 //         }
 //     }
-    
 //     levelOrder(root){
 //         let queue = []
 //         queue.push(this.root)
-//         while(queue.length){
+//         while(queue.length > 0){
 //             let current = queue.shift()
 //             console.log(current.value)
 //             if(current.left){
@@ -296,7 +89,6 @@
 //             return this.min(root.left)
 //         }
 //     }
-    
 //     max(root){
 //         if(!root.right){
 //             return root.value
@@ -304,204 +96,39 @@
 //             return this.max(root.right)
 //         }
 //     }
-    
 //     delete(value){
-//         this.root = this.deleteNode(this.root,value)
+//         this.root = this.deleteNode(this.root, value)
 //     }
-    
 //     deleteNode(root, value){
-//         if(root == null){
-//             return root
+//         if(!root){
+//             return null
 //         }
 //         if(root.value > value){
-//                     root.left = this.deleteNode(root.left, value)
-//         } else if(root.value < value){
+//             root.left =  this.deleteNode(root.left, value)
+//         } else if (root.value < value){
 //             root.right = this.deleteNode(root.right, value)
 //         } else {
-//             if(!root.left && !root.right){
-//                 return null
-//             }
             
 //             if(!root.left){
 //                 return root.right
-//             } else if(!root.right){
+//             }
+//             if(!root.right){
 //                 return root.left
 //             }
+            
 //             root.value = this.min(root.right)
 //             root.right = this.deleteNode(root.right, root.value)
 //         }
 //         return root
 //     }
-    
-// }
-
-
-// const bst = new binarySearchTree()
-// bst.insert(10)
-// bst.insert(15)
-// bst.insert(5)
-// bst.insert(3)
-// bst.insert(12)
-
-// console.log(bst.search(bst.root,110))
-// bst.postOrder(bst.root)
-// console.log(bst.max(bst.root))
-
-// bst.levelOrder(bst.root)
-
-// bst.delete(10)
-// bst.delete(7)
-// bst.levelOrder(bst.root)
-
-
-
-
-
-
-
-//--------------------------------------------------------SECOND LARGEST
-
-
-// class Node {
-//     constructor(value){
-//         this.value = value
-//         this.left = null
-//         this.right = null
-//     }
-// }
-
-// class binarySearchTree {
-//     constructor(){
-//         this.root = null
-//     }
-    
-//     insert(value){
-//         const node = new Node(value)
-//         if(this.root == null){
-//             this.root = node
-//         } else {
-//             this.insertNode(this.root, node)
-//         }
-//     }
-    
-//     insertNode(root,node){
-//         if(root.value > node.value){
-//             if(root.left == null){
-//                 root.left = node
-//             } else {
-//                 this.insertNode(root.left, node)
-//             }
-//         } else {
-//             if(root.right == null){
-//                 root.right = node
-//             } else {
-//                 this.insertNode(root.right, node)
-//             }
-//         }
-//     }
-    
-//     search(root, value){
-//         if(!root){
-//             return false
-//         } else {
-//             if(root.value == value){
-//                 return true
-//             } else if(root.value > value){
-//                 return this.search(root.left, value)
-//             } else {
-//                 return this.search(root.right, value)
-//             }
-//         }
-//     }
-    
-//     preOrder(root){
-//         if(root){
-//             console.log(root.value)
-//             this.preOrder(root.left)
-//             this.preOrder(root.right)
-//         }
-//     }
-    
-//     inOrder(root){
-//         if(root){
-//             this.inOrder(root.left)
-//             console.log(root.value)
-//             this.inOrder(root.right)
-//         }
-//     }
-    
-//     postOrder(root){
-//         if(root){
-//             this.postOrder(root.left)
-//             this.postOrder(root.right)
-//             console.log(root.value)
-//         }
-//     }
-    
-//     levelOrder(root){
-//         let queue = []
-//         queue.push(root)
-//         while(queue.length){
-//             let current = queue.shift()
-//             console.log(current.value)
-//             if(current.left){
-//                 queue.push(current.left)
-//             }
-//             if(current.right){
-//                 queue.push(current.right)
-//             }
-//         }
-//     }
-    
-//     min(root){
-//         if(root.left){
-//            return this.min(root.left) 
-//         } else {
-//             return root.value
-//         }
-//     }
-    
-//     max(root){
-//         if(!root.right){
-//             return root.value
-//         } else {
-//             return this.max(root.right)
-//         }
-//     }
-    
-//     delete(value){
-//         this.root = this.deleteNode(this.root,value)
-//     }
-    
-//     deleteNode(root,value){
-//         if(root == null){
-//             return root
-//         }
-//         if(root.value > value){
-//             root.left = this.deleteNode(root.left, value)
-//         } else if(root.value < value){
-//             root.right = this.deleteNode(root.right, value)
-//         } else {
-//             if(!root.left && !root.right){
-//                 return null
-//             }
-            
-//             if(!root.left){
-//                 return root.right
-//             } else if (!root.right){
-//                 return root.right
-//             }
-//             root.value = this.min(root.right)
-//             root.right = this.deleteNode(root.right, root.value)
-//         }
-//     }
-    
 //     secondLargest(root){
 //         if(!root || (!root.left && !root.right)){
-//             return null
+//             return false
 //         }
 //         let curr = root
+//         let parr = null
 //         while(curr.right){
+//             parr = curr
 //             curr = curr.right
 //         }
         
@@ -512,67 +139,76 @@
 //             }
 //             return curr.value
 //         }
-        
-//         let parent = root 
-//         while(parent.right !== curr){
-//             parent = parent.right
-//         }
-//         return parent.value
+//         return parr ? parr.value : null
 //     }
-    
-    
 // }
 
 
-
 // const bst = new binarySearchTree()
+// bst.insert(20)
+// bst.insert(25)
+// bst.insert(30)
 // bst.insert(10)
 // bst.insert(5)
-// bst.insert(15)
-// bst.insert(3)
 // bst.insert(7)
-// bst.insert(14)
-// console.log(bst.search(bst.root,900))
-
-// bst.inOrder(bst.root)
 
 // console.log(bst.secondLargest(bst.root))
 
 
+// console.log(bst.search(bst.root, 10))
+// bst.inOrder(bst.root)
+// console.log(bst.search(bst.root, 10))
+// bst.delete(10)
+// console.log(bst.search(bst.root, 10))
+// bst.inOrder(bst.root)
+// console.log(bst.search(bst.root, 10))
+// console.log(bst.max(bst.root))
 
 
 
 
 
-class Node {
-    constructor(value){
-        this.value = value
-        this.left = null
-        this.right = null
-    }
-}
-
-function same (first, second){
-    if(first == null && second == null){
-        return true
-    }
-    if(first == null || second == null){
-        return false
-    }
-    return (first.value == second.value)&&
-    same(first.left , second.left)&&
-    same(first.right , second.right)
-}
 
 
-const first = new Node(10)
-first.left = new Node(5)
-first.right = new Node(15)
-first.left.left = new Node(3)
 
-const second = new Node(10)
-second.left = new Node(5)
-second.left.left = new Node(3)
-second.right = new Node(15)
 
-console.log(same(first, second))
+
+
+//--------------------------------------------------------DUPLICATE TREE----------------
+
+
+// class Tree {
+//     constructor(value){
+//         this.value = value
+//         this.left = null
+//         this.right = null
+//     }
+// }
+
+// function same(first, second){
+//     if(first == null && second == null){
+//         return true
+//     }
+//     if(first == null || second == null){
+//         return false
+//     }
+
+//     return (first.value == second.value)&&
+//     same(first.left, second.left)&&
+//     same(first.right, second.right)
+// }
+
+
+// const first = new Tree(10)
+// first.left = new Tree(5)
+// first.left.left = new Tree(3)
+// first.right = new Tree(15)
+
+// const second = new Tree(10)
+// second.left = new Tree(5)
+// second.left.left = new Tree(3)
+// second.right = new Tree(15)
+
+
+
+// console.log(same(first, second))
