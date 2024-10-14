11ed49add3d7a61a4e96afd38bb5d2d72938254f
fayeemismail <faheemmuhammed703@gmail.com>
2024-10-14 19:17:05 +0530

    heap

--- heap.js
+++ heap.js
@@ -50,46 +50,46 @@
 //-------------------------------------------------MAX HEAP
 
 
-class maxHeap {
-    constructor(){
-        this.heap = []
-    }
-    getParent(index){
-        return Math.floor((index - 1)/2)
-    }
-    getLeftChild(index){
-        return 2 * index + 1
-    }
-    getRightChild(index){
-        return 2 * index + 2
-    }
-    swap(index1, index2){
-        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]]
-    }
-    insert(value){
-        this.heap.push(value)
-        this.heapUp()
-    }
-    heapUp(){
-        let index = this.heap.length -1
-        while(index > 0){
-            let parent = this.getParent(index)
-            if(this.heap[index] > this.heap[parent]){
-                this.swap(index, parent)
-                index = parent
-            } else {
-                break
-            }
-        }
-    }
-    getMax(){
-        return this.heap
-    }
-}
-const max = new maxHeap()
-max.insert(2)
-max.insert(3)
-max.insert(4)
-max.insert(5)
-max.insert(1)
-console.log(max.getMax())
+// class maxHeap {
+//     constructor(){
+//         this.heap = []
+//     }
+//     getParent(index){
+//         return Math.floor((index - 1)/2)
+//     }
+//     getLeftChild(index){
+//         return 2 * index + 1
+//     }
+//     getRightChild(index){
+//         return 2 * index + 2
+//     }
+//     swap(index1, index2){
+//         [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]]
+//     }
+//     insert(value){
+//         this.heap.push(value)
+//         this.heapUp()
+//     }
+//     heapUp(){
+//         let index = this.heap.length -1
+//         while(index > 0){
+//             let parent = this.getParent(index)
+//             if(this.heap[index] > this.heap[parent]){
+//                 this.swap(index, parent)
+//                 index = parent
+//             } else {
+//                 break
+//             }
+//         }
+//     }
+//     getMax(){
+//         return this.heap
+//     }
+// }
+// const max = new maxHeap()
+// max.insert(2)
+// max.insert(3)
+// max.insert(4)
+// max.insert(5)
+// max.insert(1)
+// console.log(max.getMax())
